a1e472826e698f581a49a8876df03bd578936f9c
Abdelrhman-Elnagar <AbdelrahmanElnagar@hotmail.com>
2024-11-20 16:18:25 +0200

    add controller , views and routes for auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->controller(AuthController::class)
+    ->group(function () {
+        Route::get('login','login')->name('auth.login');
+        Route::get('reg','reg')->name('auth.reg');
+        Route::get('fPass','fPass')->name('auth.fPass');
+    });
